5aebf2ff7c88268ef463e29dbf2afeb371a3d852
maxtuzz <maxtuzzolino@gmail.com>
2020-05-07 17:13:09 +1200

    added placeholder light theme

---
+++ client/src/styling/lightTheme.ts
@@ -0,0 +1,17 @@
+import {DefaultTheme} from "./styled";
+
+// This absolutely sucks lol
+const lightTheme: DefaultTheme = {
+    colors: {
+        sidebarColor: "#8ba4c2",
+        sidebarHighlight: "rgba(5,5,5,0.05)",
+
+        main: "#86a1c2",
+        secondary: "#7e9ab8",
+        backgroundColor: "#869dcb",
+        active: "#1b3c42",
+        words: "#404042"
+    },
+}
+
+export default lightTheme;
