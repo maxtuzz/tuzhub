4091f2a7b5dfd4ef475c2e44a663234ebfb41d34
maxtuzz <maxtuzzolino@gmail.com>
2020-06-03 14:35:38 +1200

    - add knative properties

--- server/src/main/kotlin/io/tuzzy/portal/config/AppGlobal.kt
+++ server/src/main/kotlin/io/tuzzy/portal/config/AppGlobal.kt
@@ -12,6 +12,30 @@ class AppGlobal {
             return Config.get("app.name", "tuzzy-api")
         }
 
+        /**
+         * Returns the google k8 service name
+         */
+        fun kServiceName(): String? {
+            return Config.get("app.k_service")
+        }
+
+        /**
+         * Google k8 cloud run
+         */
+        fun isKService(): Boolean {
+            return kServiceName() != null
+        }
+
+        /**
+         * Google k8 app version
+         */
+        fun kRevision(): String? {
+            return Config.get("app.k_revision")
+        }
+
+        /**
+         * Determines if remote/dynamic updating is running
+         */
         fun remoteEnabled(): Boolean {
             return Config.getBool("tuzhub.enableRemoteUpdates", true)
         }
